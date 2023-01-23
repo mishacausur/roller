0533ab7dce82303a319178f6fd7ce1b88e760d46
Misha Causur <mishacausur@mail.ru>
2023-01-24 00:12:13 +0500

    base logic implemented

--- app/src/main/java/com/mishacausur/roller/MainActivity.kt
+++ app/src/main/java/com/mishacausur/roller/MainActivity.kt
@@ -3,6 +3,10 @@ package com.mishacausur.roller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.awt.font.TextAttribute
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.roll_button)
         button.text = "Let's roll"
+        button.setOnClickListener {
+            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val textView: TextView = findViewById(R.id.textview)
+        val value = Random.nextInt( 6) + 1
+        textView.text = value.toString()
     }
 }
